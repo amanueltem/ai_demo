7cb8c14089db514005eed56e0c2f9e7b5085e6c7
amanueltem <amanueltemesgen62@gmail.com>
2025-07-13 22:04:26 +0300

    Change to ollama spring library.

--- src/main/java/com/aman/ai_demo/chat/ChatController.java
+++ src/main/java/com/aman/ai_demo/chat/ChatController.java
@@ -1,40 +1,36 @@
 package com.aman.ai_demo.chat;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.ollama.api.OllamaModel;
+import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Map;
 
 @RestController
 public class ChatController {
+	 private final OllamaChatModel chatModel;
 
-    private final RestTemplate restTemplate = new RestTemplate();
-
+	    @Autowired
+	    public ChatController(OllamaChatModel chatModel) {
+	        this.chatModel = chatModel;
+	    }
     @PostMapping("/phi")
-    public String callPhi(@RequestBody String text) {
-        String prompt =text;
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        Map<String, Object> requestBody = Map.of(
-            "model", "phi",
-            "prompt", prompt,
-            "stream", false
-        );
-
-        HttpEntity<Map<String, Object>> request = new HttpEntity<>(requestBody, headers);
-
-        ResponseEntity<Map> response = restTemplate.postForEntity(
-            "http://localhost:11434/api/generate", request, Map.class);
-
-        // Extract the generated text from the response
-        return response.getBody().get("response").toString();
+    public ChatResponse callPhi(@RequestBody String text) {
+    	ChatResponse response = chatModel.call(
+    		    new Prompt(
+    		        text,
+    		        OllamaOptions.builder()
+    		            .model(OllamaModel.PHI)
+    		            .temperature(0.4)
+    		            .build()
+    		    ));
+    	return response;
     }
+    
+    
+    
 }
